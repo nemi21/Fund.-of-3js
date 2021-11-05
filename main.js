01bfe49c8ad271f280428dfc3fe485a7490f7f2a
nemi21 <48768261+nemi21@users.noreply.github.com>
2021-11-05 14:32:25 -0400

    Update main.js

--- main.js
+++ main.js
@@ -26,18 +26,31 @@ function main() {
     const boxDepth = 1;
     const geometry = new THREE.BoxGeometry(boxWidth, boxHeight, boxDepth);
 
-    const material = new THREE.MeshPhongMaterial({color: 0x44aa88}) //greenish blue
+    function makeInstance(geometry, color, x){
+        const material = new THREE.MeshPhongMaterial({color});
 
-    const cube = new THREE.Mesh(geometry, material);
-    scene.add(cube);
+        const cube = new THREE.Mesh(geometry, material);
+        scene.add(cube);
+        
+        cube.position.x = x;
+        return cube;
+    }
 
-    
+    const cubes = [
+        makeInstance(geometry, 0x44aa88, 0),
+        makeInstance(geometry, 0x8844aa, -2),
+        makeInstance(geometry, 0xaa8844, 2),  
+    ];
 
     function render(time) {
         time *= 0.001; // convert time to seconds
 
-        cube.rotation.x = time;
-        cube.rotation.y = time;
+        cubes.forEach((cube, ndx) => {
+            const speed = 1 + ndx * .1;
+            const rot = time * speed;
+            cube.rotation.x = rot;
+            cube.rotation.y = rot;
+        });
 
         renderer.render(scene, camera);
 
