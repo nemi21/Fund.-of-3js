70c8c939c7f35f58bd22e738042dc13f14617814
nemi21 <48768261+nemi21@users.noreply.github.com>
2021-11-12 14:03:53 -0500

    Update main.js

--- main.js
+++ main.js
@@ -4,14 +4,15 @@ function main() {
     const canvas = document.querySelector("#c");
     const renderer = new THREE.WebGLRenderer({canvas});
 
-    const fov = 75;
+    const fov = 40;
     const aspect = 2 //the canvas default
     const near = 0.1;
-    const far = 5;
+    const far = 1000;
     const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-    camera.position.z = 2;
+    camera.position.z = 120;
 
     const scene = new THREE.Scene();
+    scene.background = new THREE.Color(0xAAAAAA);
 
     {
         const color = 0xFFFFFF;
@@ -19,6 +20,22 @@ function main() {
         const light = new THREE.DirectionalLight(color, intensity);
         light.position.set(-1, 2, 4);
         scene.add(light);
+    }
+
+    {
+        const color = 0xFFFFFF;
+        const intensity = 1;
+        const light = new THREE.DirectionalLight(color, intensity);
+        light.position.set(-1, 2, 4);
+        scene.add(light);
+    }
+
+    const objects = [];
+    const spread = 15;
+
+    function addObject(x, y, obj){
+        obj.position.x = x * spread;
+        obj.position.y = y * spread;
     }
 
     const boxWidth = 1;
