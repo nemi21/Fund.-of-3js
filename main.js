0707f3cbea3d669c2860574a49e0fb551b340797
nemi21 <48768261+nemi21@users.noreply.github.com>
2021-11-13 12:02:34 -0500

    Update main.js

--- main.js
+++ main.js
@@ -102,6 +102,19 @@ function main() {
         shape.moveTo(x + 2.5, y + 2.5);
         shape.bezierCurveTo(x + 2.5, y + 2.5, x + 2, y, x, y);
         shape.bezierCurveTo(x - 3, y, x - 3, y + 3.5, x - 3, y + 3.5);
+        shape.bezierCurveTo(x - 3, y + 5.5, x - 1.5, y + 7.7, x + 2.5, y + 9.5);
+        shape.bezierCurveTo(x + 6, y + 7.7, x + 8, y + 4.5, x + 8, y + 3.5);
+        shape.bezierCurveTo(x + 8, y + 3.5, x + 8, y, x + 5, y);
+        shape.bezierCurveTo(x + 3.5, y, x + 2.5, y + 2.5, x + 2.5, y + 2.5);
+
+        const extrudeSettings = {
+            steps: 2,
+            depth: 2,
+            bevelEnabled: true,
+            bevelThickness: 1,
+            bevelSize : 1,
+            bevelSegmentsL: 2,
+        };
     }
 
     const boxWidth = 1;
