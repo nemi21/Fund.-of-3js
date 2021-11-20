df1b71c5ae7ba81740c4140bb847c261dea69bc2
nemi21 <48768261+nemi21@users.noreply.github.com>
2021-11-20 15:22:22 -0500

    Update main.js

--- main.js
+++ main.js
@@ -129,7 +129,8 @@ function main() {
         }
         addSolidGeometry(0, 1, new THREE.LatheGeometry(points));
     }
-   
+
+   {
     function klein(v, u, target) {
         u *= Math.PI;
         v *= 2 * Math.PI;
@@ -141,8 +142,17 @@ function main() {
             x = 3 * Math.cos(u) * (1 + Math.sin(u)) + ( 2 * (1 - Math.cos(u) / 2)) * Math.cos(u) * Math.cos(v);
             x = -8 *  Math.sin(u) - 2 * (1 - Math.cos(u) / 2) * Math.sin(u) * Math.cos(v);
         } else{
-            x = 3 * Math.cos(u) * (1 + Math.sin(u)) + 
-        }       
+            x = 3 * Math.cos(u) * (1 + Math.sin(u)) + (2 * (1 - Mathcos(u) / 2)) * Math.cos(v + Math.PI);
+            z = -8 * Math.sin(u);
+        }  
+        
+        const y = -2 * (1 - Math.cos(u) / 2) * Math.sin(v);
+        target.set(x, y, z).multiplyScalar(0.75);
+    }
+        const slices = 25;
+        const stacks = 25;
+        addSolidGeometry(2, 1, new THREE.ParamerticGeometry(klein, slices, stacks));
+
     }
 
     const boxWidth = 1;
